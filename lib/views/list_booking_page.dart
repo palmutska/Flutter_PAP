187c16e70bac931adb5889a7ab63dde0006941e1
Luis Palma <palmutska@gmail.com>
2022-05-05 22:27:49 +0100

    Improved DataTable & created delete booking option

--- lib/views/list_booking_page.dart
+++ lib/views/list_booking_page.dart
@@ -1,98 +1,216 @@
+import 'package:app/models/meal.dart';
 import 'package:app/views/login_page.dart';
+import 'package:app/widgets/global/popup.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ListPage extends StatefulWidget {
   const ListPage({Key? key}) : super(key: key);
 
   @override
-  _ListPageState createState() => _ListPageState();
+  State<ListPage> createState() => _ListPage();
 }
 
-class _ListPageState extends State<ListPage> {
+class _ListPage extends State<ListPage> {
+  int? sortColumnIndex;
+  bool isAscending = false;
+
+  Future getData() async {
+    var ref =
+        FirebaseDatabase.instance.ref("server/bookings/" + currentCard + "/");
+
+    DataSnapshot event = await ref.get();
+    Map<String, Meal> bookings = {};
+    for (DataSnapshot value in event.children) {
+      Meal pivot = Meal(
+        data: DateTime.parse(value.child("data").value.toString()),
+        tipo: value.child("tipo").value.toString() == "almoco"
+            ? "Almoço"
+            : "Jantar",
+        local: value.child("local").value.toString() == "primeira"
+            ? "1ª secção"
+            : "2ª secção",
+      );
+      bookings[value.key!] = pivot;
+    }
+    return bookings;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Positioned(
-          top: 120,
-          child: FutureBuilder<List>(
-            future: _getBooking(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                var myList = snapshot.data!;
-                if (myList.isEmpty) {
-                  return const Text(
-                    "Ainda não tem nenhuma marcação!",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                  );
-                }
-                return DataTable(
-                  border: TableBorder.all(),
-                  dividerThickness: 3,
-                  dataRowColor: MaterialStateProperty.all(Colors.white),
-                  headingRowColor:
-                      MaterialStateProperty.all(Colors.lightBlueAccent[400]),
-                  headingTextStyle: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+    final DateFormat formatter = DateFormat("dd-MM-yyyy");
+    List<DataRow> rows = [];
+
+    return FutureBuilder(
+      future: getData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+        if (snapshot.hasData) {
+          Map<String, Meal> list = snapshot.data! as Map<String, Meal>;
+          if (list.isNotEmpty) {
+            for (var k in list.keys) {
+              rows.add(DataRow(cells: [
+                DataCell(Text(formatter.format(list[k]!.data!))),
+                DataCell(Text(list[k]!.tipo.toString())),
+                DataCell(Text(list[k]!.local.toString())),
+                DataCell(IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text(
+                            'Apagar a refeição?',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: [
+                                //!Fix this
+                                RichText(
+                                  text: TextSpan(
+                                    children: [
+                                      const TextSpan(
+                                          text:
+                                              "Tem a certeza que quer desmarcar o\n"),
+                                      TextSpan(
+                                        text: (list[k]!.tipo == "almoco"
+                                                ? "almoço"
+                                                : "jantar") +
+                                            " na " +
+                                            (list[k]!.local == "primeira"
+                                                ? "1ª secção"
+                                                : "2ª secção") +
+                                            " no dia " +
+                                            formatter.format(list[k]!.data!) +
+                                            "?",
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          actions: [
+                            ElevatedButton.icon(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.green),
+                              ),
+                              label: const Text("Confirmar"),
+                              icon: const Icon(Icons.check_box_outlined),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                if (DateTime.now()
+                                    .add(const Duration(days: 2))
+                                    .isBefore(list[k]!.data!)) {
+                                  setState(() {
+                                    list.remove(list[k]);
+                                    FirebaseDatabase.instance
+                                        .ref("server/bookings/" +
+                                            currentCard +
+                                            "/" +
+                                            k)
+                                        .remove();
+                                  });
+                                } else {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => ShowPopup(
+                                      buildContext: context,
+                                      msg:
+                                          'Já passou o tempo limite para desmarcar esta refeição!',
+                                      title: 'Ops!',
+                                    ),
+                                  );
+                                }
+                              },
+                            ),
+                            ElevatedButton.icon(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.red),
+                              ),
+                              label: const Text("Cancelar"),
+                              icon: const Icon(Icons.cancel_outlined),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                )),
+              ]));
+            }
+          } else {
+            return const Center(
+              child: Text("Sem dados"),
+            );
+          }
+          return Center(
+            child: SizedBox(
+              width: 600,
+              height: 400,
+              child: DataTable(
+                headingTextStyle: const TextStyle(fontWeight: FontWeight.bold),
+                border: TableBorder.symmetric(
+                  inside: const BorderSide(width: 1),
+                ),
+                sortAscending: isAscending,
+                sortColumnIndex: sortColumnIndex,
+                columns: [
+                  DataColumn(
+                    label: Row(children: const [
+                      Text("Data "),
+                      Icon(Icons.calendar_month_outlined)
+                    ]),
                   ),
-                  columns: const [
-                    DataColumn(label: Text("Data")),
-                    DataColumn(label: Text("Tipo")),
-                    DataColumn(label: Text("Local")),
-                  ],
-                  rows: createRows(myList),
-                );
-              } else {
-                return const CircularProgressIndicator();
-              }
-            },
-          ),
-        ),
-      ],
+                  DataColumn(
+                    onSort: (columnIndex, asceding) {
+                      //? Tentar fazer o sort
+                      setState(() {
+                        sortColumnIndex = columnIndex;
+                        isAscending = asceding;
+                      });
+                    },
+                    label: Row(children: const [
+                      Text("Tipo "),
+                      Icon(Icons.local_dining_outlined)
+                    ]),
+                  ),
+                  DataColumn(
+                    label: Row(children: const [
+                      Text("Local "),
+                      Icon(Icons.place_outlined)
+                    ]),
+                  ),
+                  DataColumn(
+                      label: Row(
+                    children: const [
+                      Text("Ações "),
+                      Icon(Icons.more_horiz_outlined)
+                    ],
+                  )),
+                ],
+                rows: rows,
+              ),
+            ),
+          );
+        }
+        return const Center(
+          child: Text("Sem dados"),
+        );
+      },
     );
   }
-}
-
-Future<List> _getBooking() async {
-  // ignore: prefer_typing_uninitialized_variables
-  var ref =
-      FirebaseDatabase.instance.ref("server/bookings/" + currentCard + "/");
-  var bookings = [];
-
-  try {
-    DataSnapshot event = await ref.get();
-    for (DataSnapshot value in event.children) {
-      var pivot = [];
-      pivot.add(value.child("data").value);
-      pivot.add(value.child("tipo").value);
-      pivot.add(value.child("local").value);
-      bookings.add(pivot);
-    }
-  } catch (err) {
-    if (kDebugMode) {
-      print("Erro: " + err.toString());
-    }
-  }
-  return bookings;
-}
-
-List<DataRow> createRows(List<dynamic> list) {
-  List<DataRow> rows = [];
-  for (var value in list) {
-    rows.add(DataRow(
-      cells: [
-        DataCell(Text(value[0])),
-        DataCell(Text(value[1])),
-        DataCell(Text(value[2])),
-      ],
-    ));
-  }
-  return rows;
 }
